8856b1fd1d0e77ec017f194cd834fd9470913ac3
yacine <yacine.yousfi@ens.uvsq.fr>
2021-04-05 15:43:34 +0200

    Implementation de la classe Interpreteur

---
+++ src/main/java/fr/uvsq/poo/compte/Interpreteur.java
@@ -0,0 +1,26 @@
+package fr.uvsq.poo.compte;
+
+import java.util.Stack;
+
+public class Interpreteur {
+    private Stack<Undo> historique;
+    private Joystick interp;
+
+    public Interpreteur() {
+        this.historique = new Stack<Undo>();
+        this.interp= new Joystick();
+    }
+
+    private void executeCommand(Command command) {
+        command.excuter();
+    }
+    public void applyStoreCommand(Undo command) {
+            command.excuter();
+            this.historique.add(command);
+    }
+    public void shutdown() {
+        this.executeCommand(new Quit(interp));
+    }
+
+
+}
