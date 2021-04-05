6217842aa8dbc547e73ac0082b77e1bc5076f87b
uvsq21921420 <ram.yann@yahoo.fr>
2021-04-05 15:02:26 +0200

    implementation de la class quit

--- src/main/java/fr/uvsq/poo/compte/Quit.java
+++ src/main/java/fr/uvsq/poo/compte/Quit.java
@@ -1,2 +1,14 @@
 package fr.uvsq.poo.compte;
 
+public class Quit implements Command {
+
+
+    private Joystick joykey;
+
+    public Quit(Joystick key) {
+        this.joykey = key;
+    }
+    public void excuter() {
+        joykey.turnOff();
+    }
+}
