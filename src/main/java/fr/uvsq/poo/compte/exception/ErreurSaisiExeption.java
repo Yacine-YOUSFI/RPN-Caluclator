23ddd6a2cb333db55bcb5e92dc53d797da23db37
yacine <yacine.yousfi@ens.uvsq.fr>
2021-04-11 19:16:37 +0200

    Implementation de la classe ErreurSaisiExeption qui permet de detecter une erreur de saisi

---
+++ src/main/java/fr/uvsq/poo/compte/exception/ErreurSaisiExeption.java
@@ -0,0 +1,11 @@
+package fr.uvsq.poo.compte.exception;
+
+public class ErreurSaisiExeption extends IllegalArgumentException{
+    /**
+     * Constructeur de l'exception
+     */
+    public ErreurSaisiExeption() {
+        super("Erreur de saisie.");
+    }
+
+}
