896ba21d8be7d02cbd84dbef7009f5d79cd45c6a
yacine <yacine.yousfi@ens.uvsq.fr>
2021-04-11 17:06:51 +0200

    Documentation de la classe Enregistrer Commande

--- src/main/java/fr/uvsq/poo/compte/EnregistrerCommand.java
+++ src/main/java/fr/uvsq/poo/compte/EnregistrerCommand.java
@@ -3,23 +3,35 @@ package fr.uvsq.poo.compte;
 import java.util.Stack;
 
 /**
+ *
  * @author YOUSFI Yacine
  */
 
 public class EnregistrerCommand implements Command {
     private Integer nb;
     private Stack<Integer> pile;
-// Cette classe permet de stocker les opérandes dans la pile
+
+    /**
+     * Constructeur de la classe EnregistrerCommand
+     * @param pile
+     * @param nb
+     */
 
     public EnregistrerCommand(Stack<Integer> pile, Integer nb) {
         this.pile = pile;
         this.nb = nb;
     }
+
+    /**
+     * Empilement d'un entier dans une pile
+     */
     public void excuter() {
         this.pile.push(nb);
     }
 
-
+    /**
+     * Cette méthode permet de dépiler un élément de la pile
+     */
     public void undo() {
         this.pile.pop();
     }
